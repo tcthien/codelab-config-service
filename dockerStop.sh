10e8f286d61dfcecc8b978373d4d3da24cb04d31
Thien Tran <thientran1986@gmail.com>
2017-02-11 16:44:48 +0700

    Stop & remove docker daemon thread

--- dockerStop.sh
+++ dockerStop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker ps -a --no-trunc | grep ${1} | awk '{print $1}' | xargs docker stop
-docker ps -a --no-trunc | grep ${1} | awk '{print $1}' | xargs docker rm
+docker ps -a --no-trunc | grep ${1} | awk '{print $1}' | xargs docker stop | xargs docker rm
