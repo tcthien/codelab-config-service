c7d101f57e9db99711ba6a177fcdd4585a488337
Thien Tran <thientran1986@gmail.com>
2017-02-11 08:56:28 +0700

    Moving codelab-config from code-lab to singleton folder

---
+++ src/main/java/com/tts/codelab/CodelabConfigApplication.java
@@ -0,0 +1,14 @@
+package com.tts.codelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CodelabConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodelabConfigApplication.class, args);
+	}
+}
