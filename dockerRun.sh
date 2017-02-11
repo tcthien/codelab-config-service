e2aa8f0b55e9159c4e07fcb80c80d34495913bba
Thien Tran <thientran1986@gmail.com>
2017-02-11 16:14:02 +0700

    Remove --rm

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -p 20088:20088 --rm --name=codelab-config --env-file=./env codelab-config
+docker run -d -p 20088:20088 --name=codelab-config --env-file=./env codelab-config
