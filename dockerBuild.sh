3112ebbb04733a7ef96440c7b359c05784afd50a
Thien Tran <thientran1986@gmail.com>
2017-02-11 16:27:31 +0700

    update docker build script

--- dockerBuild.sh
+++ dockerBuild.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t codelab-config .
+docker build -t ${1} .
--- dockerRun.sh
+++ dockerRun.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -d -p 20088:20088 --name=codelab-config --env-file=./env codelab-config
+docker ps --no-trunc | grep ${1} | awk '{print $1}' | xargs docker stop  
+
+docker run -d -p 20088:20088 --name=${1} --env-file=./env ${1}
