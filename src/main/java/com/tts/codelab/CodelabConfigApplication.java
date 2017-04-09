f922d9cf86f34f6b98bc9059a0c6cd1df5f1149a
Thien Tran <thientran1986@gmail.com>
2017-04-09 05:10:43 +0000

    Minor update

--- src/main/java/com/tts/codelab/CodelabConfigApplication.java
+++ src/main/java/com/tts/codelab/CodelabConfigApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class CodelabConfigApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CodelabConfigApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CodelabConfigApplication.class, args);
+    }
 }
